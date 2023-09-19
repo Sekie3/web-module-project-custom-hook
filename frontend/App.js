35df913a16e4eeb1769b96bdea0d7426075821bb
Tyler <sekie0@gmail.com>
2023-09-18 19:56:05 -0600

    s9 m3

--- frontend/App.js
+++ frontend/App.js
@@ -4,12 +4,14 @@ import axios from "axios";
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 
+import useDarkMode from "./Hooks/useDarkMode";
+
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
-    axios
+    axios 
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
